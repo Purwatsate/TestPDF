c0d68189335dadcb83cfa2f5f59d57def188d6c2
Aung Naing Tun <aungnaing.tun@partnerassociates.com>
2025-06-12 23:09:19 +0630

    update

--- Program.cs
+++ Program.cs
@@ -7,22 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 QuestPDF.Settings.License = LicenseType.Community;
 // true by default
 // QuestPDF.Settings.UseEnvironmentFonts = false;
-FontManager.RegisterFont(File.OpenRead("Fonts/Padauk-Regular.ttf"));
+FontManager.RegisterFont(File.OpenRead("Fonts/Pyidaungsu.ttf"));
 
 var app = builder.Build();
 
 app.MapGet("/pdf", () =>
 {
-    // Create the PDF
     var document = Document.Create(container =>
     {
         container.Page(page =>
         {
             page.Margin(50);
             page.Size(PageSizes.A4);
-
             page.Content().Text("သီဟိုဠ်မှ ဉာဏ်ကြီးရှင်သည် အာယုဝဍ္ဎနဆေးညွှန်းစာကို ဇလွန်ဈေးဘေး ဗာဒံပင်ထက် အဓိဋ္ဌာန်လျက် ဂဃနဏဖတ်ခဲ့သည်။")
-                          .FontFamily("Padauk")
+                          .FontFamily("Pyidaungsu")
                           .FontSize(24);
         });
     });
@@ -34,5 +32,5 @@ app.MapGet("/pdf", () =>
 
     return Results.File(stream.ToArray(), "application/pdf", "output.pdf");
 });
-app.Urls.Add("http://0.0.0.0:5087"); 
+app.Urls.Add("http://0.0.0.0:5087");
 app.Run();
